7183a9dfd2d6a7837712a8b58dc656ae3ef3b90f
RayDarar <dfqgth400@gmail.com>
2020-02-05 20:45:00 +0600

    server prepared

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
