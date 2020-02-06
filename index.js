708cbbd8c8883264d13e1a60d3a669d895434be2
RayDarar <dfqgth400@gmail.com>
2020-02-06 11:05:23 +0600

    router history mode fixes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
+
+
 express()
+  .get('/*', (req, res) => res.sendFile('index.html'))
   .use(express.static(path.join(__dirname, "public")))
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
