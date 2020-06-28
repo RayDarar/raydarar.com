a0a7c794e202e9a49f20ec2c0fdcca744c84e308
RayDarar <dfqgth400@gmail.com>
2020-06-28 20:16:56 +0600

    health api implemented

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 require("dotenv").config();
+const http = require("http");
 
 const express = require("express");
-const join = require("path").join;
-const history = require("connect-history-api-fallback");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const STATIC = join(__dirname, "public");
 
-app
-  .use(history())
-  .use(express.static(STATIC))
-  .listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+app.use((req, res, next) => {
+  console.log(`${req.method} request at ${req.url}`);
+  next();
+});
+
+app.get("/api/health", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/api/health/sum", (req, res) => {
+  const { first, second } = req.query;
+  console.log(req.query);
+  
+  res.send({
+    result: parseInt(first) + parseInt(second),
   });
+});
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server runs at port ${PORT}`));
+
+process.on("SIGINT", () => {
+  server.close();
+});
