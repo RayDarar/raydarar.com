4f1453676b15afd0787ce2569ffe5e62b9ef18fc
RayDarar <dfqgth400@gmail.com>
2020-06-28 17:26:45 +0600

    fixing lock file

--- app/test/main.js
+++
@@ -1,8 +0,0 @@
-const config = require("../src/config");
-const expect = require("chai").expect;
-
-describe("configuration", () => {
-  it("must have production enabled", () => {
-    expect(config.isProd).to.be.equal(true);
-  });
-});
