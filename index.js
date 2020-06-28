aa210c706c6ea6b637369015875cc2cbde408b8b
RayDarar <dfqgth400@gmail.com>
2020-06-28 17:03:03 +0600

    pm2 config file added

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const join = require("path").join;
 const history = require("connect-history-api-fallback");
-// const enforce = require("express-sslify");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const STATIC = join(__dirname, "public");
-
-// enforce https
-// if (process.env.PROD) app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app
   .use(history())
