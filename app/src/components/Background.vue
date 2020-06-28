80e6a860388b13e1b489352ed9747fd56f61c281
RayDarar <dfqgth400@gmail.com>
2020-06-28 21:12:09 +0600

    background refactored

--- app/src/components/Background.vue
+++ app/src/components/Background.vue
@@ -1,27 +1,74 @@
 <template>
-  <div id="canvas"></div>
+  <div id="canvas">
+    <div id="stars-1"></div>
+    <div id="stars-2"></div>
+    <div id="stars-3"></div>
+  </div>
 </template>
 
-<script>
-import sky from "@/helpers/NightSky";
+<style lang="scss" scoped>
+/*
+  Credits for the idea to: 
+  @screenshake | linkedin.com/in/saranshsinha
+*/
 
-export default {
-  methods: {},
-  mounted() {
-    sky.createCanvas("canvas");
-    sky.setBackgroundColors("#123578", "#1A2639");
-
-    if (this.$root.isProd) {
-      sky.setNumberOfStars(500);
-      sky.setFramerate(24);
-    } else {
-      sky.setNumberOfStars(1);
-      sky.setFramerate(1);
-    }
-
-    sky.startSimulation();
+// n is number of stars required
+@function multiple-box-shadow($n) {
+  $value: "#{random(2000)}px #{random(2000)}px #fff";
+  @for $i from 2 through $n {
+    $value: "#{$value} , #{random(2000)}px #{random(2000)}px #FFF";
   }
-};
-</script>
 
-<style lang="scss" scoped></style>
+  @return unquote($value);
+}
+
+$n: 400;
+
+$shadows-small: multiple-box-shadow($n);
+$shadows-medium: multiple-box-shadow($n / 2);
+$shadows-big: multiple-box-shadow($n / 3);
+
+#canvas {
+  height: 100%;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  overflow: hidden;
+}
+
+@mixin starsGeneral($size, $animationTime, $shadows) {
+  width: $size;
+  height: $size;
+  background: transparent;
+  box-shadow: $shadows;
+  animation: moveUp $animationTime linear infinite;
+  &:after {
+    content: " ";
+    position: absolute;
+    top: 2000px;
+    width: $size;
+    height: $size;
+    background: transparent;
+    box-shadow: $shadows;
+  }
+}
+
+#stars-1 {
+  @include starsGeneral(1px, 50s, $shadows-small);
+}
+
+#stars-2 {
+  @include starsGeneral(2px, 100s, $shadows-medium);
+}
+
+#stars-3 {
+  @include starsGeneral(3px, 150s, $shadows-big);
+}
+
+@keyframes moveUp {
+  from {
+    transform: translateY(0px);
+  }
+  to {
+    transform: translateY(-2000px);
+  }
+}
+</style>
