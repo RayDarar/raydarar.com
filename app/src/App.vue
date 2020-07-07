3107c029e02d010e6b1c9ab18cc933f00fe5f1d4
RayDarar <dfqgth400@gmail.com>
2020-07-07 20:34:42 +0600

    fix: socket over https

--- app/src/App.vue
+++ app/src/App.vue
@@ -51,7 +51,8 @@ import IO from "socket.io-client";
 import config from "@/config";
 
 const io = IO(
-  config.isProd ? "https://162.243.165.83:3000/" : "http://localhost:3000"
+  config.isProd ? "https://162.243.165.83:3000/" : "http://localhost:3000",
+  { secure: config.isProd }
 );
 
 const WRAP_TIMEOUT = 500;
