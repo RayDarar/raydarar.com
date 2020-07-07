672ac09d11081fbd4470bd83740b164b7e8004bc
RayDarar <dfqgth400@gmail.com>
2020-07-07 20:02:31 +0600

    feature: socket.io integrated to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -47,6 +47,12 @@
 <script>
 import Nav from "@/components/Navigation";
 import Background from "@/components/Background";
+import IO from "socket.io-client";
+import config from "@/config";
+
+const io = IO(
+  config.isProd ? "https://162.243.165.83:3000/" : "http://localhost:3000"
+);
 
 const WRAP_TIMEOUT = 500;
 
--- app/src/main.js
+++ app/src/main.js
@@ -9,14 +9,14 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: (h) => h(App),
   methods: {
     getImgUrl(image) {
       const images = require.context("@/assets/", false, /\.svg|\.jpg$/);
       return images("./" + image);
-    }
+    },
   },
   data: {
-    isProd: config.isProd
-  }
+    isProd: config.isProd,
+  },
 }).$mount("#app");
